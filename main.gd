5ecec3f987b7445b0b1d3d753873fc909edef65e
Marcelo Cohen <mflashbr@gmail.com>
2026-09-14 18:49:36 -0300

    Exemplos de movimentos usando teclado e mouse

--- main.gd
+++ main.gd
@@ -1,12 +1,26 @@
 extends Node2D
 
 var total : float = 0
+const SPEED : int = 100
 
 func _ready() -> void:
 	update_score(total)
-	
-func _process(delta: float) -> void:
+
+func _input(event: InputEvent) -> void:
+	pass
+	#print(event.as_text())
+	#if event.is_action_pressed("ui_right"):
+	#	print("Right arrow")
+		
+func _physics_process(delta: float) -> void:
 	print(delta)
+	if Input.is_action_pressed("ui_right"):
+		position.x += SPEED * delta
+	elif Input.is_action_pressed("ui_left"):
+		position.x -= SPEED * delta
+		
+func _process(delta: float) -> void:
+	#print(delta)
 	total += delta
 	update_score(total)
 	
